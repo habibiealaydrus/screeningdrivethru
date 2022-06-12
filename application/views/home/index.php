01603f20267cc5abe9f9f46c1dc766115282f0fc
habibiealaydrus <habibiemohamad@gmail.com>
2022-06-12 08:24:06 +0700

    layouting home

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,4 +1,12 @@
-
-    <h1>Screening Drive Thru</h1>
-
-   
+<div class="row">
+    <div class="col-12">
+        <div class="headerindex">
+            <div class="textheader rounded-3">
+                <h1>
+                    <strong>Welcome to Full Digitalized Clinic</strong>
+                </h1>
+                <h3>Experience The Next New Health Service For Your Convinion</h3>
+            </div> 
+        </div>
+    </div>
+</div>
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -7,13 +7,13 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-
+    <link href="<?php base_url(); ?>assets/css/mystyles.css" type="text/css" rel="stylesheet" >
     <title>Screening Drive Thru</title>
   </head>
   <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Digital Clinic</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
